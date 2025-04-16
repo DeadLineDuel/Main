52b639c3409ead3f9a159015ceba37f9d3524770
bbpasd <bbpasd@gmail.com>
2025-04-16 11:10:14 +0900

    Add LobbyUI
    로딩에 따른 UI 텍스트 set,
    AI에게 물어본 SceneAsync 임시 코드

---
+++ Assets/KTW/Scripts/UI/UI_CharacterSelect.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CharacterSelect : MonoBehaviour {
+    [Header("UI Assign")]
+    [SerializeField] private List<Button> characterButtons;
+    [SerializeField] private Image selectedCharacterBorderImage;
+    [SerializeField] private Button matchmakingButton;
+    [SerializeField] private Button cancleMatchmakingButton;
+    [SerializeField] private TextMeshProUGUI matchingText;
+    [SerializeField] private GameObject matchingSpinnerObject;
+
+    [Header("Matchmaking")]
+    [SerializeField] private int selectedCharacterIndex = -1;
+    [SerializeField] private bool isMatchmaking = false;
+
+    [Header("Sound")]
+    [SerializeField] private AudioClip characterSelectSound;
+    [SerializeField] private AudioClip matchmakingSound;    // TODO 사운드 추가
+    private AudioSource audioSource;
+
+    private void Start() {
+        InitalizedButtons();
+        UpdateMatchmakingUI(false);
+        selectedCharacterBorderImage.gameObject.SetActive(false);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void InitalizedButtons() {
+        matchmakingButton.onClick.AddListener(() => OnClickMatchmakingtButton());
+        cancleMatchmakingButton.onClick.AddListener(() => OnClickCanclematchMakingtButton());
+
+        for (int i = 0; i < characterButtons.Count; i++) {
+            int index = i; // Value Capture
+            characterButtons[i].onClick.AddListener(() => OnCharacterSelected(index));
+        }
+    }
+
+    private void UpdateMatchmakingUI(bool isMatching) {
+        matchmakingButton.gameObject.SetActive(!isMatching);
+        cancleMatchmakingButton.gameObject.SetActive(isMatching);
+        matchingText.gameObject.SetActive(isMatching);
+        matchingSpinnerObject.gameObject.SetActive(isMatching);
+    }
+
+    private void OnClickMatchmakingtButton() {
+        if (selectedCharacterIndex == -1) return;
+
+        Debug.Log("매칭 버튼 클릭");
+        UpdateMatchmakingUI(true);
+        isMatchmaking = true;
+
+        // PlayMatchmakingSound(matchmakingSound);
+        // TODO 매치메이킹
+
+    }
+
+    private void OnClickCanclematchMakingtButton() {
+        Debug.Log("매칭 취소 버튼 클릭");
+        UpdateMatchmakingUI(false);
+        isMatchmaking = false;
+        // TODO 매칭 취소
+    }
+
+    private void OnCharacterSelected(int index) {
+        if (isMatchmaking) return;
+
+        Debug.Log($"캐릭터 {index + 1} 선택");
+
+        selectedCharacterIndex = index;
+
+        // 빨간 테두리 옮기기 (선택 효과)
+        if (selectedCharacterBorderImage != null) {
+            selectedCharacterBorderImage.gameObject.SetActive(true);
+            Vector3 buttonPosition = characterButtons[index].GetComponent<RectTransform>().position;
+            selectedCharacterBorderImage.rectTransform.position = buttonPosition;
+        }
+    }
+
+    // 외부에서 값 가져갈 때?
+    public int GetSelectedCharacter() {
+        return selectedCharacterIndex;
+    }
+
+    private void PlayMatchmakingSound(AudioClip clip) {
+        if (audioSource == null) return;
+        if (clip == null) return;
+
+        audioSource.PlayOneShot(clip);
+    }
+}
--- Assets/KTW/Scripts/UI/UI_Lobby.cs
+++ Assets/KTW/Scripts/UI/UI_Lobby.cs
@@ -2,93 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class UI_Lobby : MonoBehaviour {
+
+public class UI_Lobby : MonoBehaviour
+{
     [Header("UI Assign")]
-    [SerializeField] private List<Button> characterButtons;
-    [SerializeField] private Image selectedCharacterBorderImage;
-    [SerializeField] private Button matchmakingButton;
-    [SerializeField] private Button cancleMatchmakingButton;
-    [SerializeField] private TextMeshProUGUI matchingText;
-    [SerializeField] private GameObject matchingSpinnerObject;
+    public TMP_Text readyText;
 
-    [Header("Matchmaking")]
-    [SerializeField] private int selectedCharacterIndex = -1;
-    [SerializeField] private bool isMatchmaking = false;
-
-    [Header("Sound")]
-    [SerializeField] private AudioClip characterSelectSound;
-    [SerializeField] private AudioClip matchmakingSound;    // TODO 사운드 추가
-    private AudioSource audioSource;
-
-    private void Start() {
-        InitalizedButtons();
-        UpdateMatchmakingUI(false);
-        selectedCharacterBorderImage.gameObject.SetActive(false);
-        audioSource = GetComponent<AudioSource>();
+    public void SetPlayerReadyStatus(int ready, int total) {
+        readyText.text = $"Players Loading : {ready}/{total}"; // 일단 2가 아니라 total 사용
     }
 
-    private void InitalizedButtons() {
-        matchmakingButton.onClick.AddListener(() => OnClickMatchmakingtButton());
-        cancleMatchmakingButton.onClick.AddListener(() => OnClickCanclematchMakingtButton());
+    public void SetAllReady() {
+        readyText.text = "All players ready!";
+    }
 
-        for (int i = 0; i < characterButtons.Count; i++) {
-            int index = i; // Value Capture
-            characterButtons[i].onClick.AddListener(() => OnCharacterSelected(index));
+    public void SetCountdown(int seconds) {
+        if (seconds == 1) {
+            readyText.text = $"All players ready! Start after {seconds} second"; ;
         }
-    }
-
-    private void UpdateMatchmakingUI(bool isMatching) {
-        matchmakingButton.gameObject.SetActive(!isMatching);
-        cancleMatchmakingButton.gameObject.SetActive(isMatching);
-        matchingText.gameObject.SetActive(isMatching);
-        matchingSpinnerObject.gameObject.SetActive(isMatching);
-    }
-
-    private void OnClickMatchmakingtButton() {
-        if (selectedCharacterIndex == -1) return;
-
-        Debug.Log("매칭 버튼 클릭");
-        UpdateMatchmakingUI(true);
-        isMatchmaking = true;
-
-        // PlayMatchmakingSound(matchmakingSound);
-        // TODO 매치메이킹
-
-    }
-
-    private void OnClickCanclematchMakingtButton() {
-        Debug.Log("매칭 취소 버튼 클릭");
-        UpdateMatchmakingUI(false);
-        isMatchmaking = false;
-        // TODO 매칭 취소
-    }
-
-    private void OnCharacterSelected(int index) {
-        if (isMatchmaking) return;
-
-        Debug.Log($"캐릭터 {index + 1} 선택");
-
-        selectedCharacterIndex = index;
-
-        // 빨간 테두리 옮기기 (선택 효과)
-        if (selectedCharacterBorderImage != null) {
-            selectedCharacterBorderImage.gameObject.SetActive(true);
-            Vector3 buttonPosition = characterButtons[index].GetComponent<RectTransform>().position;
-            selectedCharacterBorderImage.rectTransform.position = buttonPosition;
+        else {
+            readyText.text = $"All players ready! Start after {seconds} seconds"; ;
         }
+            
     }
 
-    // 외부에서 값 가져갈 때?
-    public int GetSelectedCharacter() {
-        return selectedCharacterIndex;
-    }
 
-    private void PlayMatchmakingSound(AudioClip clip) {
-        if (audioSource == null) return;
-        if (clip == null) return;
-
-        audioSource.PlayOneShot(clip);
-    }
 }
