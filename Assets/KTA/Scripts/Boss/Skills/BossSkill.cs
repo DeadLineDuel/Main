0b01ca876231aec2d4ef7bfce77e14d3470171fc
TaeAhnK <tenny0124@naver.com>
2025-04-17 14:27:47 +0900

    Add Boss Skill Sample with Indicator

--- Assets/KTA/Scripts/Boss/Skills/BossSkill.cs
+++ Assets/KTA/Scripts/Boss/Skills/BossSkill.cs
@@ -5,15 +5,23 @@ namespace Boss.Skills
 {
     public abstract class BossSkill : MonoBehaviour
     {
-        [field: SerializeField] public String BossSkillName { get; private set; }
-
+        [field: SerializeField] public String BossSkillName { get; protected set; }
+        [field: SerializeField] public SkillIndicator SkillIndicator { get; protected set; }
         public int BossSkillHash { get; private set; }
 
         protected virtual void Awake()
         {
            BossSkillHash =  Animator.StringToHash(BossSkillName);
+           if (SkillIndicator)
+           {
+               SkillIndicator.OnIndicatorComplete += OnIndicatorComplete;
+           }
         }
-
-        public abstract void Perform();
+        
+        public abstract void Perform(Vector3 targetPosition);
+        protected abstract void PerformIndicator();
+        protected abstract void OnIndicatorComplete();
+        protected abstract void PerformCollider();
+        protected abstract void PerformParticle();
     }
 }
--- Assets/KTA/Scripts/Boss/Skills/SampleBossSkill.cs
+++ Assets/KTA/Scripts/Boss/Skills/SampleBossSkill.cs
@@ -1,12 +1,73 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 namespace Boss.Skills
 {
     public class SampleBossSkill : BossSkill
     {
-        public override void Perform()
+        [field: SerializeField] private float radius;
+        private Collider[] colliders = new Collider[2];
+        private Vector3 targetPosition;
+        public override void Perform(Vector3 targetPosition)
         {
+            this.targetPosition = targetPosition;
+            gameObject.transform.position = targetPosition;
+            PerformIndicator();
+        }
+
+        protected override void PerformIndicator()
+        {
+            SkillIndicator.ActivateIndicator(targetPosition, 180f, 0.5f);
+        }
+
+        protected override void OnIndicatorComplete()
+        {
+            PerformCollider();
+            PerformParticle();
+        }
+
+        protected override void PerformCollider()
+        {
+            var size = Physics.OverlapSphereNonAlloc(targetPosition, radius, colliders);
             
+            Vector3 forward = transform.forward;
+
+            if (size > 0)
+            {
+                foreach (var col in colliders)
+                {
+                    Vector3 dir = (col.transform.position - targetPosition).normalized;
+                    float angle = Vector3.Angle(forward, dir);
+                    if (angle <= 90f) // 180도(반원) 이내
+                    {
+                        Debug.Log(col.gameObject.name);
+                    }
+                }   
+            }
+        }
+
+        protected override void PerformParticle()
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(targetPosition, radius);
+
+            // 반원(180도) 외곽선 그리기 예시
+            int segments = 32;
+            float angleStep = 180f / segments;
+            Vector3 forward = transform.forward;
+            for (int i = 0; i < segments; i++)
+            {
+                float angleA = -90f + angleStep * i;
+                float angleB = -90f + angleStep * (i + 1);
+                Vector3 dirA = Quaternion.Euler(0, angleA, 0) * forward;
+                Vector3 dirB = Quaternion.Euler(0, angleB, 0) * forward;
+                Gizmos.DrawLine(targetPosition + dirA * radius, targetPosition + dirB * radius);
+            }
         }
     }
 }
