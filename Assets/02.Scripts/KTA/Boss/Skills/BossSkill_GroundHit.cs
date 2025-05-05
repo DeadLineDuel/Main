4016c632e4330f6cdc34e0dfe83877bb3cc0ab7b
TaeAhnK <tenny0124@naver.com>
2025-05-06 01:02:43 +0900

    Add BossSkill - GroundHit

---
+++ Assets/02.Scripts/KTA/Boss/Skills/BossSkill_GroundHit.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using Unity.Netcode;
+using System.Collections;
+
+namespace Boss.Skills
+{
+    public class BossSkill_GroundHit : BossSkill
+    {
+        [field: Header("Dependencies")]
+        [field: SerializeField] private ParticleSystem skillEffectParticle;
+        [field: SerializeField] private SkillIndicator skillIndicator;
+        
+        [field: Header("Skill Data")]
+        [field: SerializeField] private float radius;
+        
+        private Vector3 bossPos;
+
+        public override void ActivateSkill()
+        {
+            if (!IsServer) return;  // On Server
+            
+            bossPos = BossCore.transform.position;
+            StartCoroutine(ExecuteSkillSequence());
+        }
+
+        private IEnumerator ExecuteSkillSequence()
+        {
+            BossCore.NetworkAnimator.SetTrigger(BossSkillHash);
+            
+            // Play Indicator
+            yield return new WaitForSeconds(IndicatorTime);
+            ActivateIndicatorClientRpc();
+            
+            // Play Effect and Damage Collider
+            yield return new WaitForSeconds(EffectTime);
+            ActivateSkillEffectClientRpc();
+            ActivateDamageCollider(BossCore.BossStats.Atk.Value);
+        }
+        
+        [ClientRpc]
+        public override void ActivateIndicatorClientRpc()
+        {
+            if (!BossCore.BossCharacter.IsClientBoss) return;
+            skillIndicator.ActivateIndicator(bossPos, 360f, 1.6f, 0f);
+        }
+
+        [ClientRpc]
+        public override void ActivateSkillEffectClientRpc()
+        {
+            if (!BossCore.BossCharacter.IsClientBoss) return;
+            skillEffectParticle.Play();
+        }
+
+        public override void ActivateDamageCollider(float bossAtk)
+        {
+            if (!IsServer) return;
+            
+            int layerMask = LayerMask.GetMask("Player");
+            var size = Physics.OverlapSphereNonAlloc(bossPos, radius, Colliders, layerMask);
+                
+            Vector3 forward = transform.forward;
+                
+            if (size > 0)
+            {
+                for  (int i = 0; i < size; i++) // Do not use foreach on NonAlloc
+                {
+                    Debug.Log("[Boss] Hit Object : " + Colliders[i].gameObject.name);
+                    if (Colliders[i].TryGetComponent<IDamageable>(out var damageable))
+                    {
+                        damageable.TakeDamage(bossAtk * damageCoeff);
+                    }
+                    
+                }   
+            }
+        }
+        
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(bossPos, radius);
+        }
+    }
+}
